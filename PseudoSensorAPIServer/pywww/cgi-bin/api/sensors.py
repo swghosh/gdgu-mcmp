cf784ea994d6cb9597577f96233a16ec7f5d6e53
SwG Ghosh <codecrafts.cf@icloud.com>
2018-05-16 01:12:40 +0530

    Added description in pseudosensorapiserver/pywww/cgi-bin/sensors.py

--- PseudoSensorAPIServer/pywww/cgi-bin/api/sensors.py
+++ PseudoSensorAPIServer/pywww/cgi-bin/api/sensors.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+""" 
+Python module containing the following functions:
+    * mockedSensor function to generate a dictionary of sensor data
+        of the format:
+        { 
+            timestamp: isoDateString, latitude: coordinate, longitude: coordinate, sensorChannel: channelIdString,
+            senorData: {
+                parameter1: value1, parameter2: value2, ..., parameterN: valueN
+            }
+        }
+    * do function will print a line of 'Content-Type: application/json' for CGI 
+        and print a json string of the dictionary containing sensor data generated using mockedSensor
+"""
+
 import datetime
 import json
 import random
