38fba40a6078677ec72fc58ebc659c597c923496
SwG Ghosh <codecrafts.cf@icloud.com>
2018-05-16 01:18:21 +0530

    Added description in pseudosensorapiserver/pseudosensorapiwebserver.py

--- PseudoSensorAPIServer/pseudosensorapiwebserver.py
+++ PseudoSensorAPIServer/pseudosensorapiwebserver.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+    Python program to run a simple HTTP web server capable of handling CGI scripts
+        that serves files from the ./pywww directory and 
+        executable scripts over CGI from the ./pywww/cgi-bin directory
+"""
+
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
