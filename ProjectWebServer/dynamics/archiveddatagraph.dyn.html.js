769bed9ea070f1c8d0d711cd7b017b7452439500
SwG Ghosh <codecrafts.cf@icloud.com>
2018-08-11 02:18:24 +0530

    Added demo minimal graph

---
+++ ProjectWebServer/dynamics/archiveddatagraph.dyn.html.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var path = require('path')
+var htmldyn = require('../lib/htmldynmodule')
+
+exports.serve = (request, response) => {
+    fs.readFile(path.join(__dirname, '../files', 'template.html'), 'utf8', (err, templateData) => {
+
+        fs.readFile(path.join(__dirname, '../files', 'archiveddatagraph.html'), 'utf8', (err, contentData) => {
+
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+    
+            response.end(htmldyn.getHtmlStringWithIdValues(templateData, {
+                content: contentData
+            }), 'utf-8')
+
+        })
+
+    })
+}
---
+++ ProjectWebServer/static/chartjsinit.js
@@ -0,0 +1,18 @@
+// generates a chart using ChartJS to render a graph of prices and fluctuation of an individual item
+var chartGenerate = function(labels, values) {
+    // the canvas item of a specific id which will be used for chart generate
+    var ctx = document.getElementById("chart").getContext('2d');
+    // new chartJS object with the required canvas as reference
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels, // labels with times
+            datasets: [{
+                lineTension: 0,
+                data: values, // values with different prices of fluctuations
+                backgroundColor: "rgba(100, 100, 100, 0.1)",
+                borderColor: "#679"
+            }]
+        }
+    });
+};
