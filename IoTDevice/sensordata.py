c0a8d992839d1af9285ef8156ef46d392670818e
SwG Ghosh <codecrafts.cf@icloud.com>
2018-10-21 15:30:31 +0530

    Added accuracy in IoTDevice/sensordata.py

--- IoTDevice/sensordata.py
+++ IoTDevice/sensordata.py
@@ -3,12 +3,14 @@ from datetime import datetime
 # GDGU co-ordinates
 LATITUDE = 28.2699415
 LONGITUDE = 77.0636767
+ACCURACY = 100
 
 def getData(kind = 'default'):
     return {
         'timestamp': datetime.utcnow().isoformat(),
         'latitude': LATITUDE,
         'longitude': LONGITUDE,
+        'accuracy': ACCURACY,
         'sensorData': {
             kind: {
                 'value': 100,
