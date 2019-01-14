49103e15fbb4190bcacd7212ef085a80bfcf74af
Swarup Ghosh <codecrafts.cf@icloud.com>
2019-01-15 00:45:26 +0530

    Moved deprecated folder to branch 'deprecated'

--- deprecated/ProjectWebServer/lib/objectformatters.js
+++
@@ -1,5 +0,0 @@
-exports.objectFormatterA = anObject => Object.values(anObject).join(' ')
-
-exports.objectFormatterB = anObject => Object.keys(anObject)
-                                              .map(key => `${key} ${objectFormatterA(anObject[key])}`)
-                                              .join('; ')
